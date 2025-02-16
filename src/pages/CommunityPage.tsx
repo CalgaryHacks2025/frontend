b99462e6a73c8fe39f001b2b29a003617b86c6e7
isabellegan <78586330+isabellegan@users.noreply.github.com>
2025-02-16 12:12:52 -0500

    created components for date + location (shortened) and volunteer count for community events on Home and Community pages

--- src/pages/CommunityPage.tsx
+++ src/pages/CommunityPage.tsx
@@ -2,22 +2,33 @@ import { Link } from "react-router-dom";
 import Nav from "../components/Nav";
 import MainHeader from "../components/MainHeader";
 import animalShelterCleanupDay from "../assets/animal-shelter-clean-up-day.png";
+import CalendarInfo from "../components/CalendarInfo";
+import CommunityNumbers from "../components/CommunityNumbers";
 
 const CommunityPage = () => {
   return (
     <>
       <MainHeader />
 
-      <div>
-        <article className="flex justify-center py-8">
+      <div className="px-6 pt-7">
+        <h1 className="font-bold pb-5.5">Upcoming Events</h1>
+
+        <article className="flex justify-center">
           <img
             src={animalShelterCleanupDay}
             alt="nurse holding sick dog"
-            className="w-9/10 rounded-md object-cover"
+            className="w-full rounded-tl-md rounded-tr-md rounded-bl-none rounded-br-none object-cover"
           />
         </article>
 
-        <section></section>
+        <section className="bg-[#F1F0EA] px-6 py-5 rounded-bl-md rounded-br-md rounded-tl-none rounded-tr-none">
+          <h2 className="font-bold pb-3 pt-1">Animal Shelter Clean-up Day</h2>
+
+          <div>
+            <CalendarInfo date="March 15, 2025" location="Pet City Shelter" />
+            <CommunityNumbers spots={23} />
+          </div>
+        </section>
       </div>
 
       <button>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -33,7 +33,6 @@ export const HomePage = () => {
           </div>
         </section>
       </div>
-
       <Nav />
     </>
   );
