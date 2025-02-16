03bbac8fb07841fd00f043277caac9541545304a
Elizabeth Ko <elizabeth_ko7@hotmail.com>
2025-02-16 09:49:40 -0500

    Fixed the secondaryheader.tsx so that the back button works and the title is editable

--- src/App.tsx
+++ src/App.tsx
@@ -5,39 +5,24 @@ import AdoptPage from "./pages/AdoptPage";
 import CommunityPage from "./pages/CommunityPage";
 import ProfilePage from "./pages/ProfilePage";
 import HomePage from "./pages/HomePage";
-<<<<<<< HEAD
 import UpdatesPage from "./pages/UpdatesPage";
-=======
 import AdoptDetailsPage from "./pages/AdoptDetailsPage";
->>>>>>> develop
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/assess" element={<AssessPage />} />
           <Route path="/adopt" element={<AdoptPage />} />
+          <Route path="/adopt/:id" element={<AdoptDetailsPage />} />
+
           <Route path="/community" element={<CommunityPage />} />
           <Route path="/profile/:id" element={<ProfilePage />} />
           <Route path="/updates" element={<UpdatesPage />} />
         </Routes>
       </BrowserRouter>
-=======
-    <BrowserRouter>
-      <Routes>
-      <Route path="/" element={<HomePage />} />
-        <Route path="/assess" element={<AssessPage />} />
-        <Route path="/adopt" element={<AdoptPage />} />
-        <Route path="/adopt/:id" element={<AdoptDetailsPage />} />
-
-        <Route path="/community" element={<CommunityPage />} />
-        <Route path="/profile/:id" element={<ProfilePage />} />
-      </Routes>
-    </BrowserRouter>
->>>>>>> develop
     </>
   );
 }
