9225067a710276459345d3044a469494d35048dc
isabellegan <78586330+isabellegan@users.noreply.github.com>
2025-02-16 09:58:37 -0500

    nav for nested pages

--- src/pages/CommunityPage.tsx
+++ src/pages/CommunityPage.tsx
@@ -1,12 +1,24 @@
-import Nav from "../components/Nav"
+import Nav from "../components/Nav";
+import arrowLeft from "../assets/arrow-left.svg";
 
 const CommunityPage = () => {
   return (
     <>
-        <div>Community</div>
-        <Nav/>
+      <div>
+        <div className="flex items-center py-8 px-6 border-b">
+          <img
+            src={arrowLeft}
+            className="w-5 h-5 cursor-pointer"
+            alt="left-arrow"
+          />
+          <div className="flex-grow text-center">
+            <h1>Event Details</h1>
+          </div>
+        </div>
+      </div>
+      <Nav />
     </>
-  )
-}
+  );
+};
 
-export default CommunityPage
+export default CommunityPage;
