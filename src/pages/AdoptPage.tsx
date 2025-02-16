b459a0b910299039d95ff6b41f70415643b2f1bd
Rintaro Masuda <66854464+rmasuda12@users.noreply.github.com>
2025-02-16 04:15:25 -0700

    connect to backend server - .env file, and api call attempts

--- src/pages/AdoptPage.tsx
+++ src/pages/AdoptPage.tsx
@@ -1,8 +1,24 @@
 import Nav from "../components/Nav"
 import MainHeader from "../components/MainHeader"
 import AnimalCard from "../components/AnimalCard"
+import axios from "axios"
+import { useEffect } from "react"
 
 const AdoptPage = () => {
+  const baseURL = import.meta.env.VITE_API_URL;
+  console.log(baseURL)
+
+  async function getAnimals() {
+    try {
+      const results = await axios.get(`${baseURL}/animals`);
+      console.log(results);
+    } catch (error) {
+      console.log('some issues with getting animal')
+    }
+
+  }
+
+  useEffect(()=>{getAnimals()},[])
   
 
   return (
