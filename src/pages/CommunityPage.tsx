9585b92d8a35288ae597721e34fcb6d5c89e7d15
Rintaro Masuda <66854464+rmasuda12@users.noreply.github.com>
2025-02-16 10:02:31 -0700

    Revert "added Community Activities Near You and ensured full responsiveness a…"

--- src/pages/CommunityPage.tsx
+++ src/pages/CommunityPage.tsx
@@ -1,25 +1,11 @@
 import { Link } from "react-router-dom";
 import Nav from "../components/Nav";
 import MainHeader from "../components/MainHeader";
-import animalShelterCleanupDay from "../assets/animal-shelter-clean-up-day.png";
 
 const CommunityPage = () => {
   return (
     <>
       <MainHeader />
-
-      <div>
-        <article className="flex justify-center py-8">
-          <img
-            src={animalShelterCleanupDay}
-            alt="nurse holding sick dog"
-            className="w-9/10 rounded-md object-cover"
-          />
-        </article>
-
-        <section></section>
-      </div>
-
       <button>
         <Link to="/event-details">Join Event</Link>
       </button>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,42 +1,15 @@
-import MainHeader from "../components/MainHeader";
-import Nav from "../components/Nav";
-import animalShelterCleanupDay from "../assets/animal-shelter-clean-up-day.png";
-import spotsRemainingIcon from "../assets/community-gray.svg";
-MainHeader;
+import MainHeader from "../components/MainHeader"
+import Nav from "../components/Nav"
+MainHeader
 
 export const HomePage = () => {
   return (
     <>
-      <MainHeader />
-      <div className="px-6 pt-7">
-        <h1 className="font-bold pb-5.5">Community Activities Near You</h1>
-
-        <article className="flex justify-center">
-          <img
-            src={animalShelterCleanupDay}
-            alt="nurse holding sick dog"
-            className="w-full rounded-tl-md rounded-tr-md rounded-bl-none rounded-br-none object-cover"
-          />
-        </article>
-
-        <section className="bg-[#F1F0EA] px-6 py-5 rounded-bl-md rounded-br-md rounded-tl-none rounded-tr-none">
-          <h2 className="font-bold pb-1.5 pt-1">Animal Shelter Clean-up Day</h2>
-          <div className="flex items-center pb-1">
-            <img
-              src={spotsRemainingIcon}
-              alt="spots remaining icon"
-              className="w-5 h-5 cursor-pointer mr-3"
-            />
-            <div>
-              <p>23 volunteers</p>
-            </div>
-          </div>
-        </section>
-      </div>
-
-      <Nav />
+        <MainHeader/>
+        <div>HomePage</div>
+        <Nav/>
     </>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default HomePage
